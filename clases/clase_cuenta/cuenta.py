974a1b4e4c0fb27dcae8e4be2ec3ae384053f588
fran855 <65423343+fran855@users.noreply.github.com>
2022-07-14 20:41:24 -0300

    Cambios

--- clases/clase_cuenta/cuenta.py
+++ clases/clase_cuenta/cuenta.py
@@ -1,7 +1,8 @@
 class Cuenta:
     def __init__(self, **kwargs):
         self.monto = 0
-        self.limite_extraccion_diario = kwargs.get("limite_extraccion_diario", 0)
+        
+        """ self.limite_extraccion_diario = kwargs.get("limite_extraccion_diario", 0)
         self.limite_monto_transferido = kwargs.get("limite_monto_transferido", 0)
         self.porcentaje_comision_trasnferencia = kwargs.get("porcentaje_comision_transferencia", 0)
         self.descubierto_cuenta_corriente = kwargs.get("descubierto_cuenta_corriente", 0)
@@ -13,4 +14,5 @@ class Cuenta:
         self.cantidad_cuenta_corriente_pesos = kwargs.get("cantidad_cuenta_corriente_pesos", 0)
         self.cantidad_maxima_chequeras = kwargs.get("cantidad_maxima_chequeras", 0)
         self.compra_dolares = kwargs.get("compra_dolares", False)
-        self.venta_dolares = kwargs.get("venta_dolares", False)
+        self.venta_dolares = kwargs.get("venta_dolares", False) """
+        
