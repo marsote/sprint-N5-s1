515e5218fab804587fe0a60f4ef33aa19e2fe660
fran855 <65423343+fran855@users.noreply.github.com>
2022-07-14 20:41:14 -0300

    Cambios

--- clases/clase_cliente/cliente_classic.py
+++ clases/clase_cliente/cliente_classic.py
@@ -3,3 +3,8 @@ from .cliente import Cliente
 class ClienteClassic(Cliente):
     def __init__(self, **kwargs):
         super(ClienteClassic, self).__init__(**kwargs)
+
+    def puede_crear_chequera(self):
+        return False
+
+    
